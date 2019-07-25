9f873b590a514c80eda06c025d86578a9e9fc3bf
wanchana <bawzaza103@gmail.com>
2019-07-25 16:58:57 +0700

    wanchana

---
+++ src/lab3/HelloWorld.java
@@ -0,0 +1,16 @@
+package lab3;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        int x=100 ;
+        System.out.println("Hello,World!!!\n");
+        System.out.println("\twanchana Chanratrsa");
+        System.out.println("362211760038");
+        System.out.println("bawzaza004@gmail.com");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ"+100);
+
+
+
+
+}}//class
---
+++ src/lab4/Exlnput.java
@@ -0,0 +1,29 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class Exlnput {
+
+    public static void main (String[] args){
+
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.print("Please,entered an integer:");
+         int x = scanner.nextInt();
+
+         System.out.println ("Your entered integer: "+y);
+
+         //input double
+        System.out.print("Please,enter double: ");
+        double y = scanner.nextInt();
+
+        System.out.println ("Your entered double: "+y);
+
+        //input String
+        System.out.println("Please,enter you name");
+        String s = scanner.nextLine();
+
+        System.out.println("You name is:"+s);
+
+    }
+}
