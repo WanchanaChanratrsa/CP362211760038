5b015f83587ef5f4080d385c02ca0faf754596c6
wanchana <bawzaza103@gmail.com>
2019-10-10 14:27:20 +0700

    Midterm

--- src/lab7/ex8.java
+++ src/lab7/ex8.java
@@ -3,12 +3,13 @@ package Lab7;
 public class ex8 {
     public static void main(String[] args) {
 
-        for (int i = 8; i >=1; i--) {
+        for (int i = 8; i >= 1; i--) {
             for (int j = 8; j >= i; j--) {
                 System.out.print("*");
             }//for j
-        }
-        System.out.println();
+            System.out.println();
+        }//for i
+
 
         for (int i = 1; i <=8 ; i++) {
             for (int j = 1; j <= i ; j++) {
