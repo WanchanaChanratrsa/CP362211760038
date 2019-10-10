fb0dab10d7ee5b49e070a10d1e1378184515ced6
wanchana <bawzaza103@gmail.com>
2019-10-10 15:28:29 +0700

    Midterm

--- src/lab8/ex1.java
+++ src/lab8/ex1.java
@@ -6,12 +6,19 @@ public class ex1 {
         int [] number = new  int[5];
         System.out.println(number.length);
 
+        number[4] = 10;
+        number[1] = number[4]*2;
+        number[3] = number[4] +number[1];
+        number[0] = number[2]= (number[1]+number[3]+number[4])*5;
+
+
         for (int i:number) {
             System.out.print(i+" ");
         }
         System.out.println();
         for (int i = 0; i <number.length ; i++) {
             System.out.print(number[i]+" ");
+
 
 
         }
